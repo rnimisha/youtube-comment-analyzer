ce8816371781d6189330575429bba8ceadb5b181
rnimisha <nimisaraut@gmail.com>
2025-06-01 17:10:26 +0545

    feat: look into network of reply

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from common.sentiment_analyzer import predict_sentiments
 
 def predict(url, limit=100):
     comments_df, replies_df, video_details = download_youtube_comments(url, limit)
-    result_df = predict_sentiments(
+    comment_pred_df = predict_sentiments(
         comments_df, "model/both_fine_tune_classifier_1.pt", "comment"
     )
-    return result_df, video_details
+    reply_pred_df = predict_sentiments(
+        replies_df, "model/both_fine_tune_classifier_1.pt", "reply"
+    )
+    reply_pred_df.to_csv("data/reply.csv", index=False)
+    comment_pred_df.to_csv("data/comment.csv", index=False)
+    return comment_pred_df, video_details
 
 
 if __name__ == "__main__":
     video_url = "https://www.youtube.com/watch?v=l5Z1PBJLUss"
-    print(predict(video_url, 100))
+    print(predict(video_url, 1000))
